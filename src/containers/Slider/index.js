5d5f102c2661225ad2c2fefc97a584036e562ccd
MMSG97 <maria.m.mga88@gmail.com>
2024-09-24 00:40:09 +0200

    debug du slider sur : le retour à la 1ère img L24-30; modif du 1er 'map' + fermeture avant le 2ème map + supp des balises <>,</>; chgt de idx en index pour 'checked' + '_' pour 'event'; ajout de 'readOnly' pour empêcher la modif; ajout de comms

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,11 +8,26 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    // ancien code => new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    // Correction pour afficher du plus ancien au plus récent
+    new Date(evtA.date) < new Date(evtB.date) ? - 1 : 1
   );
+  // ancien code
+  // const nextCard = () => {
+  //   setTimeout(
+  //     () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+  //     5000
+  //   );
+  // };
+
+
+  // Correction pour que le slider affiche la première image une fois arrivé à la dernière sans afficher de page blanche entre les 2
+  const dataLength = data?.focus.length
+
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      // Ajout de -1 après "length"
+      () => setIndex(index < dataLength - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -22,36 +37,39 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
             }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
+        </div>
+      ))}  {/* Ajout de la fermeture du premier ".map" */}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            // {byDateDesc.map((_, radioIdx)
+            <input
+              key={event.title}
+              // key={`${event.id}`} correction event n'a pas d'id
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              // checked={idx === radioIdx} correction idx => index 
+              readOnly
+            // "console error : Warning: You provided a `checked` prop to a form field without an `onChange` handler."
+            // Cela affichera un champ en lecture seule. Si le champ doit être modifiable, utiliser defaultChecked. Sinon, utiliser onChange ou readOnly.
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
